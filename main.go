b860863208cb717bbb07b5ac8a517773bfbdc97d
Theo Pack <tf.pack@gmail.com>
2018-01-08 18:13:29 +0100

    Formatting

--- main.go
+++ main.go
@@ -1,29 +1,29 @@
 package main
 
 import (
-  "github.com/CaliDog/certstream-go"
-  logging "github.com/op/go-logging"
+	"github.com/CaliDog/certstream-go"
+	logging "github.com/op/go-logging"
 )
 
 var log = logging.MustGetLogger("example")
 
 func main() {
-  // The false flag specifies that we don't want heartbeat messages.
-  stream, errStream := certstream.CertStreamEventStream(false)
-  for {
-    select {
-      case jq := <-stream:
-        messageType, err := jq.String("message_type")
+	// The false flag specifies that we don't want heartbeat messages.
+	stream, errStream := certstream.CertStreamEventStream(false)
+	for {
+		select {
+		case jq := <-stream:
+			messageType, err := jq.String("message_type")
 
-        if err != nil{
-          log.Fatal("Error decoding jq string")
-        }
+			if err != nil {
+				log.Fatal("Error decoding jq string")
+			}
 
-        log.Info("Message type -> ", messageType)
-        log.Info("recv: ", jq)
-      
-      case err := <-errStream:
-        log.Error(err)
-    }
-  }
+			log.Info("Message type -> ", messageType)
+			log.Info("recv: ", jq)
+
+		case err := <-errStream:
+			log.Error(err)
+		}
+	}
 }
